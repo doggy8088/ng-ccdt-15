ba7e44bf61ad120d4779e12b615d128983d5c7ce
Will <doggy.huang@gmail.com>
2024-01-04 11:19:52 +0800

    load script dynamically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-router';
+
+  ngOnInit() {
+    Promise.all([
+      loadScript('assets/js/demo/chart-area-demo.js'),
+      loadScript('assets/js/demo/chart-pie-demo.js'),
+    ])
+      .then(() => {
+        // Scripts have been loaded
+      })
+      .catch((error) => {
+        // Handle error if scripts fail to load
+      });
+  }
 }
+
+function loadScript(src: string) {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement('script');
+    script.src = src;
+    script.onload = resolve;
+    script.onerror = reject;
+    document.head.appendChild(script);
+  });
+}
+
