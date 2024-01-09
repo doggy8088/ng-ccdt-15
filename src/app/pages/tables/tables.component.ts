eed57dc49534bb74c4adcaafd0d40f03ae8cc7ce
Will <doggy.huang@gmail.com>
2024-01-09 10:33:48 +0800

    設定 TablesComponent 可以讀取 id 這個 Query String

--- src/app/pages/tables/tables.component.ts
+++ src/app/pages/tables/tables.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   templateUrl: './tables.component.html',
   styleUrls: ['./tables.component.css']
 })
 export class TablesComponent implements OnInit {
+
+  id: string | null = null;
+
+  constructor(private route: ActivatedRoute, private router: Router) { }
+
   ngOnInit(): void {
+
+    this.id = this.route.snapshot.queryParamMap.get('id');
 
     const datatablesScript = document.createElement('script');
     datatablesScript.src = 'assets/js/demo/datatables-demo.js';
