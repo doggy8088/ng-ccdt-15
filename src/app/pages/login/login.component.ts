cdff773c49707515cb3ec55d9a4ee913f9d5b6a2
Will <doggy.huang@gmail.com>
2024-01-17 10:43:20 +0800

    示範從 ts 讀取 NgModel 內容

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,19 +1,31 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { NgForm, NgModel } from '@angular/forms';
+import { debounce, debounceTime } from 'rxjs';
 
 @Component({
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit, OnDestroy {
+export class LoginComponent implements OnInit, OnDestroy, AfterViewInit {
   data: any = {
     email: '',
     password: '',
     remember: true
   };
 
+  @ViewChild('mEmail', { static: false }) mEmail!: NgModel;
+
   ngOnInit(): void {
     document.body.classList.add('bg-gradient-primary');
+  }
+
+  ngAfterViewInit(): void {
+    this.mEmail.valueChanges?.pipe(
+      debounceTime(500)
+    ).subscribe((value) => {
+      // this.mEmail.control.validator?.(this.mEmail.control);
+      console.log(value);
+    });
   }
 
   ngOnDestroy(): void {
